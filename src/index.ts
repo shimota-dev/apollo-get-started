0a811d042eac8fca5e2bc81172d240d4d1f5b1e4
Wataru Shimota <w.shimota@gmail.com>
2023-09-03 16:23:08 +0900

    Create an instance of ApolloServer

--- src/index.ts
+++ src/index.ts
@@ -34,8 +34,25 @@ const books = [
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
-const resolver = {
+const resolvers = {
   Query: {
     books: () => books,
   },
 };
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
